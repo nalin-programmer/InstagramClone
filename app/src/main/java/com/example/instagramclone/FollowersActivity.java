69e128963f0ec0ccb9c8060e90d44a7e6143e99e
NALIN AGRAWAL <54065357+nalin-programmer@users.noreply.github.com>
2020-10-05 19:27:59 +0530

    storydelete

--- app/src/main/java/com/example/instagramclone/FollowersActivity.java
+++ app/src/main/java/com/example/instagramclone/FollowersActivity.java
@@ -74,7 +74,30 @@ public class FollowersActivity extends AppCompatActivity {
             case "followers":
                 getFollowers();
                 break;
+            case "views":
+                getViews();
+                break;
         }
+    }
+
+    private void getViews(){
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference("Story")
+                .child( id ).child( getIntent().getStringExtra( "storyid" ) ).child( "views" );
+        reference.addListenerForSingleValueEvent( new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                idList.clear();
+                for (DataSnapshot snapshot1: snapshot.getChildren()){
+                    idList.add( snapshot1.getKey() );
+                }
+                showUsers();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        } );
     }
 
     private void getLikes() {
--- app/src/main/java/com/example/instagramclone/StoryActivity.java
+++ app/src/main/java/com/example/instagramclone/StoryActivity.java
@@ -3,15 +3,21 @@ package com.example.instagramclone;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
 import com.example.instagramclone.Model.Story;
 import com.example.instagramclone.Model.User;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -32,6 +38,10 @@ public class StoryActivity extends AppCompatActivity implements StoriesProgressV
     StoriesProgressView storiesProgressView;
     ImageView image, story_photo;
     TextView story_username;
+
+    LinearLayout r_seen;
+    TextView seen_number;
+    ImageView story_delete;
 
     List<String> images;
     List<String> storyids;
@@ -59,12 +69,24 @@ public class StoryActivity extends AppCompatActivity implements StoriesProgressV
         super.onCreate( savedInstanceState );
         setContentView( R.layout.activity_story );
 
+        r_seen = findViewById( R.id.r_seen );
+        seen_number = findViewById( R.id.seen_number );
+        story_delete = findViewById( R.id.story_delete );
+
         storiesProgressView = findViewById( R.id.stories );
         image = findViewById( R.id.image );
         story_photo = findViewById( R.id.story_photo );
         story_username = findViewById( R.id.story_username );
 
+        r_seen.setVisibility( View.GONE );
+        story_delete.setVisibility( View.GONE );
+
         userid = getIntent().getStringExtra( "userid" );
+
+        if(userid.equals( FirebaseAuth.getInstance().getCurrentUser().getUid() )){
+            r_seen.setVisibility( View.VISIBLE );
+            story_delete.setVisibility( View.VISIBLE );
+        }
 
         getStories( userid );
         userInfo( userid );
@@ -86,11 +108,43 @@ public class StoryActivity extends AppCompatActivity implements StoriesProgressV
             }
         } );
         skip.setOnTouchListener( onTouchListener );
+
+        r_seen.setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent( StoryActivity.this, FollowersActivity.class );
+                intent.putExtra( "id", userid );
+                intent.putExtra( "storyid", storyids.get(counter) );
+                intent.putExtra( "title", "views" );
+                startActivity( intent );
+            }
+        } );
+
+        story_delete.setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DatabaseReference reference = FirebaseDatabase.getInstance().getReference("Story")
+                        .child( userid ).child( storyids.get( counter ) );
+                reference.removeValue().addOnCompleteListener( new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if(task.isSuccessful()){
+                            Toast.makeText( StoryActivity.this,"Deleted!",Toast.LENGTH_SHORT ).show();
+                            finish();
+                        }
+                    }
+                } );
+            }
+        } );
     }
+
 
     @Override
     public void onNext() {
         Glide.with( getApplicationContext() ).load( images.get( ++counter ) ).into( image );
+
+        addView( storyids.get( counter ) );
+        seenNumber( storyids.get( counter ) );
 
     }
 
@@ -98,6 +152,7 @@ public class StoryActivity extends AppCompatActivity implements StoriesProgressV
     public void onPrev() {
         if((counter -1) < 0)return;
         Glide.with( getApplicationContext() ).load( images.get( --counter ) ).into( image );
+        seenNumber( storyids.get( counter ) );
     }
 
     @Override
@@ -149,6 +204,9 @@ public class StoryActivity extends AppCompatActivity implements StoriesProgressV
 
                 Glide.with( getApplicationContext() ).load( images.get( counter ) )
                         .into( image );
+
+                addView( storyids.get( counter ) );
+                seenNumber( storyids.get( counter ) );
             }
 
             @Override
@@ -167,6 +225,28 @@ public class StoryActivity extends AppCompatActivity implements StoriesProgressV
                 User user = snapshot.getValue( User.class );
                 Glide.with( getApplicationContext() ).load( user.getImageurl() ).into( story_photo );
                 story_username.setText( user.getUsername() );
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        } );
+    }
+
+    private void addView(String storyid){
+        FirebaseDatabase.getInstance().getReference("Story").child( userid )
+                .child( storyid ).child( "views" ).child( FirebaseAuth.getInstance().getCurrentUser().getUid() ).setValue( true );
+
+    }
+
+    private void seenNumber(String storyid){
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference("Story")
+                .child( userid ).child( storyid ).child( "views" );
+        reference.addListenerForSingleValueEvent( new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                seen_number.setText( ""+snapshot.getChildrenCount() );
             }
 
             @Override
